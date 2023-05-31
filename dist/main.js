ca18e076896d2bac2844995cf7303e800ac954d8
Ahmed Mohamed <ahmedelhagjr@gmail.com>
2023-05-30 22:45:50 -0400

    add logic to process incoming discord listings and send socket alerts to users with matching alert strings

--- dist/main.js
+++ dist/main.js
@@ -31,15 +31,181 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const socket_io_1 = require("socket.io");
+const mongoose_1 = __importDefault(require("mongoose"));
 const dotenv = __importStar(require("dotenv"));
+const http_1 = __importDefault(require("http"));
 dotenv.config();
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
+const prisma = new client_1.PrismaClient();
+const server = http_1.default.createServer();
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+// Keeping track of connected clients
+const connectedClients = new Map();
+function getAllUserAlerts() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const userAlerts = yield prisma.userAlerts.findMany({
+            select: {
+                userId: true,
+                alerts: true,
+            },
+        });
+        return userAlerts;
+    });
+}
+function preprocessAlerts(userAlerts) {
+    const processedAlerts = [];
+    for (const alert of userAlerts) {
+        const splitAlert = alert.split(",");
+        const alertType = splitAlert[0];
+        const keywords = splitAlert.slice(1);
+        const inclusionKeywords = [];
+        const exclusionKeywords = [];
+        for (const keyword of keywords) {
+            if (keyword.startsWith("-")) {
+                exclusionKeywords.push(keyword.slice(1));
+            }
+            else {
+                inclusionKeywords.push(keyword);
+            }
+        }
+        processedAlerts.push({
+            alertType,
+            inclusionKeywords,
+            exclusionKeywords,
+        });
+    }
+    return processedAlerts;
+}
+function sendAlert(matchingListing) {
+    return __awaiter(this, void 0, void 0, function* () { });
+}
+function processDiscordAlerts(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const userAlerts = yield getAllUserAlerts();
+        const body = data.content;
+        const matchedUsers = [];
+        for (const user of userAlerts) {
+            console.log(user.userId);
+            const preprocessedAlerts = preprocessAlerts(user.alerts);
+            for (const alert of preprocessedAlerts) {
+                let isMatch = true;
+                if (alert.alertType.toLowerCase() != data.postType.toLowerCase()) {
+                    continue;
+                }
+                for (const keyword of alert.inclusionKeywords) {
+                    if (!body.includes(keyword)) {
+                        isMatch = false;
+                        break;
+                    }
+                }
+                if (isMatch) {
+                    for (const keyword of alert.exclusionKeywords) {
+                        if (body.includes(keyword)) {
+                            isMatch = false;
+                            break;
+                        }
+                    }
+                }
+                if (isMatch)
+                    matchedUsers.push(user.userId);
+            }
+        }
+        console.log(matchedUsers);
+        return matchedUsers;
+    });
+}
+function processRedditAlerts(data) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const userAlerts = yield getAllUserAlerts();
+    });
+}
+function isMatching(body, alert) { }
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         yield prisma.$connect();
-        console.log("CONNECTED!");
+        console.log("Prisma Connected");
+        console.log(yield getAllUserAlerts());
+        try {
+            yield mongoose_1.default.connect(process.env.DATABASE_URL);
+            console.log("Mongoose Connected");
+            const db = mongoose_1.default.connection;
+            const discordStream = db.collection("DiscordMessage").watch();
+            const redditStream = db.collection("Post").watch();
+            let socketInstance;
+            io.on("connection", (socket) => __awaiter(this, void 0, void 0, function* () {
+                socketInstance = socket;
+                socket.on("join", (userId) => {
+                    connectedClients.set(userId, socket.id);
+                    console.log(connectedClients);
+                });
+                socket.on("disconnect", () => {
+                    const userId = connectedClients.get(socket.id);
+                    connectedClients.delete(userId);
+                    console.log(connectedClients);
+                });
+            }));
+            discordStream.on("change", (change) => __awaiter(this, void 0, void 0, function* () {
+                if (change.operationType === "insert") {
+                    const data = change.fullDocument;
+                    const userId = data.userId;
+                    console.log("Discord:", data._id);
+                    const matchedUsers = yield processDiscordAlerts({
+                        id: data._id,
+                        content: data.content,
+                        author: data.author,
+                        postType: data.postType,
+                        dateCreated: data.dateCreated,
+                        serverName: data.serverName,
+                        channelName: data.channelName,
+                    });
+                    console.log("matchd users:", matchedUsers);
+                    matchedUsers.forEach((user) => __awaiter(this, void 0, void 0, function* () {
+                        const socketId = connectedClients.get(user);
+                        const listingID = "Discord:" + data._id;
+                        console.log(socketId, listingID);
+                        console.log(user);
+                        console.log(connectedClients);
+                        if (socketId) {
+                            socketInstance.to(socketId).emit("alert", listingID);
+                            console.log("We throughhh", socketId);
+                        }
+                        // await prisma.userAlerts.update({
+                        //   where: {
+                        //     userId: userId,
+                        //   },
+                        //   data: {
+                        //     matchedListings: {
+                        //       push: listingID,
+                        //     },
+                        //   },
+                        // });
+                    }));
+                }
+            }));
+            redditStream.on("change", (change) => __awaiter(this, void 0, void 0, function* () {
+                if (change.operationType === "insert") {
+                    const data = change.fullDocument;
+                    const userId = data.userId;
+                    console.log("Reddit:", data);
+                }
+            }));
+            server.listen(3333, () => {
+                console.log("Websocket server listening on port 3333");
+            });
+        }
+        catch (err) {
+            console.log("Failed to connect through Mongoose:", err);
+        }
     });
 }
 main()
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { Socket, Server } from "socket.io";
 import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 import http from "http";
+import { RedditType, DiscordType } from "./types/types";
 dotenv.config();
 const prisma = new PrismaClient();
 
@@ -17,47 +18,161 @@ const io = new Server(server, {
 // Keeping track of connected clients
 const connectedClients = new Map();
 
-function getAllUserAlerts() {
-  return prisma.userAlerts
-    .findMany({
-      select: {
-        userId: true,
-        alerts: true,
-      },
-    })
-    .then(console.log);
+async function getAllUserAlerts(): Promise<
+  { userId: string; alerts: string[] }[]
+> {
+  const userAlerts = await prisma.userAlerts.findMany({
+    select: {
+      userId: true,
+      alerts: true,
+    },
+  });
+
+  return userAlerts;
 }
 
-async function processListing(listingType: string, data: any) {
-  // return listingType,
+function preprocessAlerts(userAlerts: string[]): {
+  alertType: string;
+  inclusionKeywords: string[];
+  exclusionKeywords: string[];
+}[] {
+  const processedAlerts = [];
+
+  for (const alert of userAlerts) {
+    const splitAlert = alert.split(",");
+    const alertType = splitAlert[0];
+    const keywords = splitAlert.slice(1);
+
+    const inclusionKeywords: string[] = [];
+    const exclusionKeywords: string[] = [];
+
+    for (const keyword of keywords) {
+      if (keyword.startsWith("-")) {
+        exclusionKeywords.push(keyword.slice(1));
+      } else {
+        inclusionKeywords.push(keyword);
+      }
+    }
+    processedAlerts.push({
+      alertType,
+      inclusionKeywords,
+      exclusionKeywords,
+    });
+  }
+  return processedAlerts;
 }
+
+async function sendAlert(matchingListing: string) {}
+
+async function processDiscordAlerts(data: DiscordType) {
+  const userAlerts = await getAllUserAlerts();
+
+  const body = data.content;
+  const matchedUsers = [];
+  for (const user of userAlerts) {
+    console.log(user.userId);
+    
+    const preprocessedAlerts = preprocessAlerts(user.alerts);
+    for (const alert of preprocessedAlerts) {
+      let isMatch = true;
+      if (alert.alertType.toLowerCase() != data.postType.toLowerCase()) {
+        continue;
+      }
+      for (const keyword of alert.inclusionKeywords) {
+        if (!body.includes(keyword)) {
+          isMatch = false;
+          break;
+        }
+      }
+      if (isMatch) {
+        for (const keyword of alert.exclusionKeywords) {
+          if (body.includes(keyword)) {
+            isMatch = false;
+            break;
+          }
+        }
+      }
+      if (isMatch) matchedUsers.push(user.userId);
+    }
+  }
+  console.log(matchedUsers);
+
+  return matchedUsers;
+}
+async function processRedditAlerts(data: RedditType) {
+  const userAlerts = await getAllUserAlerts();
+}
+
+function isMatching(body: string, alert: string) {}
 
 async function main() {
   await prisma.$connect();
   console.log("Prisma Connected");
-  await getAllUserAlerts();
+  console.log(await getAllUserAlerts());
+
   try {
     await mongoose.connect(process.env.DATABASE_URL as string);
-
     console.log("Mongoose Connected");
 
     const db = mongoose.connection;
     const discordStream = db.collection("DiscordMessage").watch();
     const redditStream = db.collection("Post").watch();
+    let socketInstance: Socket;
+
+    io.on("connection", async (socket) => {
+      socketInstance = socket;
+      socket.on("join", (userId) => {
+        connectedClients.set(userId, socket.id);
+        console.log(connectedClients);
+      });
+
+      socket.on("disconnect", () => {
+        const userId = connectedClients.get(socket.id);
+        connectedClients.delete(userId);
+        console.log(connectedClients);
+      });
+    });
 
     discordStream.on("change", async (change) => {
       if (change.operationType === "insert") {
         const data = change.fullDocument;
         const userId = data.userId;
-        console.log("Discord:", data);
+        console.log("Discord:", data._id);
+  
+        const matchedUsers = await processDiscordAlerts({
+          id: data._id,
+          content: data.content,
+          author: data.author,
+          postType: data.postType,
+          dateCreated: data.dateCreated,
+          serverName: data.serverName,
+          channelName: data.channelName,
+        });
+        console.log("matchd users:", matchedUsers);
+        
+        matchedUsers.forEach(async (user) => {
+          const socketId = connectedClients.get(user);
+          const listingID = "Discord:" + data._id;
+          console.log(socketId, listingID);
+          console.log(user);
+          console.log(connectedClients);
 
-        // // Get the specific client with matching userId
-        // const clients = await io.in(userId).fetchSockets();
-
-        // // Send data to the specific client
-        // clients.forEach((client) => {
-        //   client.emit("data", data);
-        // });
+          if (socketId) {
+            socketInstance.to(socketId).emit("alert", listingID);
+            
+            console.log("We throughhh", socketId);
+          }
+          // await prisma.userAlerts.update({
+          //   where: {
+          //     userId: userId,
+          //   },
+          //   data: {
+          //     matchedListings: {
+          //       push: listingID,
+          //     },
+          //   },
+          // });
+        });
       }
     });
 
@@ -66,35 +181,7 @@ async function main() {
         const data = change.fullDocument;
         const userId = data.userId;
         console.log("Reddit:", data);
-
-        // // Get the specific client with matching userId
-        // const clients = await io.in(userId).fetchSockets();
-
-        // // Send data to the specific client
-        // clients.forEach((client) => {
-        //   client.emit("data", data);
-        // });
       }
-    });
-
-    io.on("connection", async (socket) => {
-
-      socket.on("join", (userId) => {
-        connectedClients.set(socket.id, userId);
-        console.log(connectedClients);
-
-        userId = userId
-      });
-
-      socket.on("disconnect", () => {
-        console.log(socket.id);
-        
-        const userId = connectedClients.get(socket.id);
-        // console.log(userId + " disconnected");
-        connectedClients.delete(socket.id);
-        console.log(connectedClients);
-        
-      });
     });
 
     server.listen(3333, () => {
